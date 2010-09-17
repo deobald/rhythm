f520b0a4932f1a9e83e95cad7e85892d3e891819
Steven Deobald <steven@deobald.ca>
2010-09-17 13:28:57 -0500

    question the first

--- src/rhythm/core.clj
+++ src/rhythm/core.clj
@@ -1,19 +1,39 @@
 (ns rhythm.core)
 
 (defn welcome []
-  (println "To talk to me, type (t XYZ), where XYZ is your answer to the question.")
-  (println "A list begins with an open-parenthesis and ends with a close-parenthesis.")
-  (println "1. What is an empty list?"))
+  (println "To talk to me, type (answer XYZ), where XYZ is your answer to the question.")
+  (println "You can use this for short: (a XYZ)")
+  (println "My questions will appear in quotes.")
+  (println "A list begins with an open-parenthesis and ends with a close-parenthesis."))
 
-(defn start []
-  (welcome))
-  
 (def answer (ref ()))
-  
+(def number (ref 0))
+
 (defmacro s [form]
   (dosync (ref-set answer `~form))
   nil)
+
+(defmacro answer [form]
+  (if (= @answer `~form)
+    (println "word up")))
+    ;(question @number)
+    ;(question (+ 1 @number)))
+(def a answer)
+
+(defmacro check [attempt]
+  (= @answer `~attempt))
+
+(defn question-1 []
+  (s ())
+  "1. What is an empty list?")
   
-(defmacro t [form]
-  (= @answer `~form))
-  
+(defn question [n]
+  (dosync (ref-set number n))
+  (println "\n")
+  (condp = n
+    1 question-1))
+
+(defn start []
+  (welcome)
+  ((question 1)))
+
