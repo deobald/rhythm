32188ec6df4dc132bf953eb3f213dfc0c4f7cbb5
Steven Deobald <steven@deobald.ca>
2010-09-17 13:46:02 -0500

    I am bad at this.

--- src/rhythm/core.clj
+++ src/rhythm/core.clj
@@ -1,4 +1,5 @@
-(ns rhythm.core)
+(ns rhythm.core
+  (:use clojure.contrib.def))
 
 (defn welcome []
   (println "To talk to me, type (answer XYZ), where XYZ is your answer to the question.")
@@ -6,22 +7,15 @@
   (println "My questions will appear in quotes.")
   (println "A list begins with an open-parenthesis and ends with a close-parenthesis."))
 
-(def answer (ref ()))
+(def next-answer (ref ()))
 (def number (ref 0))
 
 (defmacro s [form]
-  (dosync (ref-set answer `~form))
+  (dosync (ref-set next-answer `~form))
   nil)
 
-(defmacro answer [form]
-  (if (= @answer `~form)
-    (println "word up")))
-    ;(question @number)
-    ;(question (+ 1 @number)))
-(def a answer)
-
 (defmacro check [attempt]
-  (= @answer `~attempt))
+  (= @next-answer `~attempt))
 
 (defn question-1 []
   (s ())
@@ -31,9 +25,20 @@
   (dosync (ref-set number n))
   (println "\n")
   (condp = n
-    1 question-1))
+    1 question-1
+    "Sorry, out of questions!"))
 
 (defn start []
   (welcome)
   ((question 1)))
 
+(defmacro answer [form]
+  (if (= @next-answer `~form)
+    (do
+      (println "Correct!")
+      (question (+ 1 @number)))
+    (do
+      (println "Sorry, not quite.")
+      (question @number))))
+
+(defalias a answer)
